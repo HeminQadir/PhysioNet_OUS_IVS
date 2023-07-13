2a22f176143c3a36fea8fd2f0aa670dfb6272e9b
Hemin <hemina.qadir@gmail.com>
2023-07-13 13:56:33 +0200

    team_code updated

--- team_code.py
+++ team_code.py
@@ -15,7 +15,6 @@ import mne
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import joblib
-
 from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -68,22 +67,46 @@ class dataset(Dataset):
     def __getitem__(self, idx):
         patient_id = self.X_files[idx]
 
-        #shuffled_index = self.indices[idx]
-        #patient_id = self.X_files[shuffled_index]
-
-        patient_metadata = load_challenge_data(self.data_folder, patient_id)
         # Extract patient features.
+        patient_metadata = load_challenge_data(self.data_folder, patient_id)
         patient_features = get_patient_features(patient_metadata)
-        #print(patient_features)
         patient_features[patient_features != patient_features] = 0.0
+        #print(patient_features)
 
+        # Extract EEG features.
+        eeg_channels = ['F3', 'P3', 'F4', 'P4']
+        group = 'EEG'
+        recording_ids = find_recording_files(self.data_folder, patient_id)
+        num_recordings = len(recording_ids)
+        if num_recordings > 0:
+            recording_id = recording_ids[-1]
+            recording_location = os.path.join(self.data_folder, patient_id, '{}_{}'.format(recording_id, group))
+            if os.path.exists(recording_location + '.hea'):
+                data, channels, sampling_frequency = load_recording_data(recording_location)
+                utility_frequency = get_utility_frequency(recording_location + '.hea')
+
+                if all(channel in channels for channel in eeg_channels):
+                    data, channels = reduce_channels(data, channels, eeg_channels)
+                    data, sampling_frequency = preprocess_data(data, sampling_frequency, utility_frequency)
+                    data = np.array([data[0, :] - data[1, :], data[2, :] - data[3, :]]) # Convert to bipolar montage: F3-P3 and F4-P4
+                    eeg_features = get_eeg_features(data, sampling_frequency).flatten()
+                else:
+                    eeg_features = float('nan') * np.ones(8) # 2 bipolar channels * 4 features / channel
+            else:
+                eeg_features = float('nan') * np.ones(8) # 2 bipolar channels * 4 features / channel
+        else:
+            eeg_features = float('nan') * np.ones(8) # 2 bipolar channels * 4 features / channel
+
+        eeg_features[eeg_features != eeg_features] = 0.0
+        eeg_features = np.array(eeg_features, dtype=np.float32)
+        fused_features = np.hstack((patient_features, eeg_features))
         # Extract labels.
         outcome = int(get_outcome(patient_metadata))
         #print(outcome)
         cpc = int(get_cpc(patient_metadata))
         #print(cpc)
 
-        x = torch.from_numpy(patient_features)
+        x = torch.from_numpy(fused_features)
         outcome = torch.tensor(outcome, dtype=torch.long)
         cpc = torch.tensor(cpc, dtype=torch.long)
         
@@ -177,6 +200,7 @@ def train_challenge_model(data_folder, model_folder, verbose=2):
 
     split_ratio = 0.1
     split = False
+    shuffle = True
 
     if split:
         X_train, X_val = load_train_val_files(data_folder, split, split_ratio)
@@ -187,7 +211,6 @@ def train_challenge_model(data_folder, model_folder, verbose=2):
 
     trainset = dataset(data_folder, X_train)
     batch_size = 32
-    shuffle = True
     train_data = DataLoader(trainset, batch_size=batch_size, shuffle=shuffle)
 
     os.makedirs(model_folder, exist_ok=True)
@@ -196,11 +219,11 @@ def train_challenge_model(data_folder, model_folder, verbose=2):
     input_vector = torch.tensor([53., 0., 1., 0., 0., 1., 1., 33.])
     input_vector = input_vector.cuda()
     # Initialize the model
-    input_size = len(input_vector)
-    hidden_size = 64 # Choose the number of hidden units
+    input_size = len(input_vector)*2
+    hidden_size = 64  # Choose the number of hidden units
     nclass = 2  # Since the output is binary (0 or 1)
     model = Multi_layers_Classifier(input_size, hidden_size, nclass).cuda()
-    print(model)
+    #print(model)
 
     model_path = Path(os.path.join(model_folder, 'model.pt'))
 
@@ -216,15 +239,15 @@ def train_challenge_model(data_folder, model_folder, verbose=2):
     # Define the loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     regression = nn.MSELoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
 
     # Training loop
-    num_epochs = 100 # Choose the number of training epochs
+    num_epochs = 60 # Choose the number of training epochs
     h_test = 1
 
     #old_precision = 0
     try:
-        for epoch in range(num_epochs):
+        for epoch in range(epoch, num_epochs + 1):
             """
             Train
             """
@@ -255,15 +278,19 @@ def train_challenge_model(data_folder, model_folder, verbose=2):
                 optimizer.step()
 
                 total_loss += loss.item()
-                #print("[{:3d}/{:3d}] loss: {:.6f}".format(epoch+1, num_epochs, loss.item()))
+                print("[{:3d}/{:3d}] loss: {:.6f}".format(epoch+1, num_epochs, loss.item()))
 
-            print("[{:3d}/{:3d}] loss: {:.6f}".format(epoch+1, num_epochs, total_loss))
+            print("="*80)
+            print("[{:3d}/{:3d}] total loss: {:.6f}".format(epoch+1, num_epochs, total_loss))
 
             """
             Test
             """
-            
-            if(epoch % h_test == h_test-1) and split:
+            if not split:
+                val_data = train_data
+                print("val data is tarin data now ...")
+
+            if(epoch % h_test == h_test-1):
                 TP = 0 
                 FP = 0
                 cpc_mse_t_list = []
@@ -311,14 +338,42 @@ def run_challenge_models(models, data_folder, patient_id, verbose):
     #print(patient_features)
     patient_features[patient_features != patient_features] = 0.0
 
+    # Extract EEG features.
+    eeg_channels = ['F3', 'P3', 'F4', 'P4']
+    group = 'EEG'
+    recording_ids = find_recording_files(data_folder, patient_id)
+    num_recordings = len(recording_ids)
+    if num_recordings > 0:
+        recording_id = recording_ids[-1]
+        recording_location = os.path.join(data_folder, patient_id, '{}_{}'.format(recording_id, group))
+        if os.path.exists(recording_location + '.hea'):
+            data, channels, sampling_frequency = load_recording_data(recording_location)
+            utility_frequency = get_utility_frequency(recording_location + '.hea')
+
+            if all(channel in channels for channel in eeg_channels):
+                data, channels = reduce_channels(data, channels, eeg_channels)
+                data, sampling_frequency = preprocess_data(data, sampling_frequency, utility_frequency)
+                data = np.array([data[0, :] - data[1, :], data[2, :] - data[3, :]]) # Convert to bipolar montage: F3-P3 and F4-P4
+                eeg_features = get_eeg_features(data, sampling_frequency).flatten()
+            else:
+                eeg_features = float('nan') * np.ones(8) # 2 bipolar channels * 4 features / channel
+        else:
+            eeg_features = float('nan') * np.ones(8) # 2 bipolar channels * 4 features / channel
+    else:
+        eeg_features = float('nan') * np.ones(8) # 2 bipolar channels * 4 features / channel
+
+    eeg_features[eeg_features != eeg_features] = 0.0
+    eeg_features = np.array(eeg_features, dtype=np.float32)
+    fused_features = np.hstack((patient_features, eeg_features))
+
+    x = torch.from_numpy(fused_features)
+    x = x.cuda()
+
     # Extract labels.
     #outcome = int(get_outcome(patient_metadata))
     #print(outcome)
     #cpc = int(get_cpc(patient_metadata))
     #print(cpc)
-
-    x = torch.from_numpy(patient_features)
-    x = x.cuda()
 
     #outcome = torch.tensor(outcome, dtype=torch.long)
     #cpc = torch.tensor(cpc, dtype=torch.long)
@@ -350,3 +405,60 @@ def run_challenge_models(models, data_folder, patient_id, verbose):
 
     return outcome, outcome_probability, cpcs
 
+
+# Preprocess data.
+def preprocess_data(data, sampling_frequency, utility_frequency):
+    # Define the bandpass frequencies.
+    passband = [0.1, 30.0]
+
+    # Promote the data to double precision because these libraries expect double precision.
+    data = np.asarray(data, dtype=np.float64)
+
+    # If the utility frequency is between bandpass frequencies, then apply a notch filter.
+    if utility_frequency is not None and passband[0] <= utility_frequency <= passband[1]:
+        data = mne.filter.notch_filter(data, sampling_frequency, utility_frequency, n_jobs=4, verbose='error')
+
+    # Apply a bandpass filter.
+    data = mne.filter.filter_data(data, sampling_frequency, passband[0], passband[1], n_jobs=4, verbose='error')
+
+    # Resample the data.
+    if sampling_frequency % 2 == 0:
+        resampling_frequency = 128
+    else:
+        resampling_frequency = 125
+    lcm = np.lcm(int(round(sampling_frequency)), int(round(resampling_frequency)))
+    up = int(round(lcm / sampling_frequency))
+    down = int(round(lcm / resampling_frequency))
+    resampling_frequency = sampling_frequency * up / down
+    data = scipy.signal.resample_poly(data, up, down, axis=1)
+
+    # Scale the data to the interval [-1, 1].
+    min_value = np.min(data)
+    max_value = np.max(data)
+    if min_value != max_value:
+        data = 2.0 / (max_value - min_value) * (data - 0.5 * (min_value + max_value))
+    else:
+        data = 0 * data
+
+    return data, resampling_frequency
+
+# Extract features from the EEG data.
+def get_eeg_features(data, sampling_frequency):
+    num_channels, num_samples = np.shape(data)
+
+    if num_samples > 0:
+        delta_psd, _ = mne.time_frequency.psd_array_welch(data, sfreq=sampling_frequency,  fmin=0.5,  fmax=8.0, verbose=False)
+        theta_psd, _ = mne.time_frequency.psd_array_welch(data, sfreq=sampling_frequency,  fmin=4.0,  fmax=8.0, verbose=False)
+        alpha_psd, _ = mne.time_frequency.psd_array_welch(data, sfreq=sampling_frequency,  fmin=8.0, fmax=12.0, verbose=False)
+        beta_psd,  _ = mne.time_frequency.psd_array_welch(data, sfreq=sampling_frequency, fmin=12.0, fmax=30.0, verbose=False)
+
+        delta_psd_mean = np.nanmean(delta_psd, axis=1)
+        theta_psd_mean = np.nanmean(theta_psd, axis=1)
+        alpha_psd_mean = np.nanmean(alpha_psd, axis=1)
+        beta_psd_mean  = np.nanmean(beta_psd,  axis=1)
+    else:
+        delta_psd_mean = theta_psd_mean = alpha_psd_mean = beta_psd_mean = float('nan') * np.ones(num_channels)
+
+    features = np.array((delta_psd_mean, theta_psd_mean, alpha_psd_mean, beta_psd_mean)).T
+
+    return features
